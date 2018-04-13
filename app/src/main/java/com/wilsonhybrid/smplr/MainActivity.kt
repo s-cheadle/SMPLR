2781e2ca100288918531e8cd220b5420033b78dd
Scott Cheadle <s.cheadle8424@student.leedsbeckett.ac.uk>
2018-04-13 22:41:17 +0100

    Read permissions & listeners
    Read permissions have been granted in the manifest and work has been started on the listeners

--- app/src/main/java/com/wilsonhybrid/smplr/MainActivity.kt
+++ app/src/main/java/com/wilsonhybrid/smplr/MainActivity.kt
@@ -2,23 +2,65 @@ package com.wilsonhybrid.smplr
 
 import android.media.AudioAttributes
 import android.media.SoundPool
-import android.support.v7.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.View.OnClickListener
 import android.widget.Button
 import android.widget.Toast
-import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
 
+    var ACTIVITY_CHOOSE_FILE:Int = 1
+
+    lateinit var soundPool:SoundPool
+
+    /**
+     * URI of the sample - used to get file
+     */
+    var uri:Uri = Uri.EMPTY
+
+    /**
+     * File path to sample
+     */
+    var samplePath:String = ""
+
+    /**
+     * Maximum number of sounds that can occur at once in the SoundPool
+     */
     val maxStreams = 4
+
+    /**
+     * A master volume for both left and right channels
+     */
     var masterVolume = 0.9f
+
+    /**
+     * Independent volume for the left channel
+     */
     var leftVolume = 0.9f
+
+    /**
+     * Independent volume for the right channel
+     */
     var rightVolume = 0.9f
-    var pan = 50 //50 is centre
+
+    /**
+     * Panning control
+     * Pan is the location of sound in stereo.
+     * 0 is hard left 100 is hard right.
+     */
+    var pan = 50
+
+    /**
+     * Soundpool audio priority
+     */
     private val priority = 1
 
+    /**
+     * Variables for the ID numbers used for the soundpool to identify the samples
+     */
     var ID1 = 0
     var ID2 = 0
     var ID3 = 0
@@ -40,44 +82,63 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /**
+         * AudioAttributes for the Soundpool
+         */
         val audioAttributes = AudioAttributes.Builder()
                 .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
                 .setFlags(AudioAttributes.FLAG_AUDIBILITY_ENFORCED)
                 .setUsage(AudioAttributes.USAGE_MEDIA)
                 .build()
 
-        val soundPool = SoundPool.Builder()
+        /**
+         * SoundPool is used to trigger the samples
+         */
+        soundPool = SoundPool.Builder()
+                .setMaxStreams(maxStreams)
                 .setAudioAttributes(audioAttributes)
                 .build()
 
+        /**
+         * OnLoadCompleteListener - displays a toast when a sample is loaded into the SoundPool
+         */
         var listener:SoundPool.OnLoadCompleteListener = SoundPool.OnLoadCompleteListener(function = { sp: SoundPool, i: Int, i1: Int ->
             Toast.makeText(this.applicationContext, "Loaded!", Toast.LENGTH_SHORT).show()
         })
-
         soundPool.setOnLoadCompleteListener(listener)
 
+
         val trigger1: Button = findViewById(R.id.trigger1)
+        /**
+         * Long press to change sample
+         */
         trigger1.setOnLongClickListener {
-            ID1 = soundPool.load(applicationContext, R.raw.kick,priority); return@setOnLongClickListener true
+            //TODO Load samples
+            return@setOnLongClickListener true
         }
-        trigger1.setOnClickListener { soundPool.play(ID1, leftVolume, rightVolume, 1, 0, 1f) }
 
 
-        val trigger2: Button = findViewById(R.id.trigger2)
-        val trigger3: Button = findViewById(R.id.trigger3)
-        val trigger4: Button = findViewById(R.id.trigger4)
-        val trigger5: Button = findViewById(R.id.trigger5)
-        val trigger6: Button = findViewById(R.id.trigger6)
-        val trigger7: Button = findViewById(R.id.trigger7)
-        val trigger8: Button = findViewById(R.id.trigger8)
-        val trigger9: Button = findViewById(R.id.trigger9)
-        val trigger10: Button = findViewById(R.id.trigger10)
-        val trigger11: Button = findViewById(R.id.trigger11)
-        val trigger12: Button = findViewById(R.id.trigger12)
-        val trigger13: Button = findViewById(R.id.trigger13)
-        val trigger14: Button = findViewById(R.id.trigger14)
-        val trigger15: Button = findViewById(R.id.trigger15)
-        val trigger16: Button = findViewById(R.id.trigger16)
+        /**
+         * Tap to trigger sample
+         */
+        trigger1.setOnClickListener()
+    }
+
+    private fun playSample(id:Int) {
+        Toast.makeText(this.applicationContext,"Playing " + id, Toast.LENGTH_SHORT).show()
+        soundPool.play(id, leftVolume, rightVolume, priority, 0, 1f)
+    }
+
+    private fun loadSample():Int{
+        return soundPool.load(samplePath,priority)
+    }
+
+    class Trigger(id:Int, sp:SoundPool):View.OnClickListener{
+        val ident:Int = id
+        val pool:SoundPool = sp
+        override fun onClick(v: View?) {
+                pool.play(ident,0.9f,0.9f,1,0,1f)
+        }
 
     }
 
